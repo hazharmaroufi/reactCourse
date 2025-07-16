55adafe7bb0c047189cb94950eba5550227a0991
Hazhar Maroufi <hazhmar@gmail.com>
2025-07-16 16:44:13 +0330

    reuseble components

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,9 +1,17 @@
 function Test() {
   return (
-    <div>
-      <p>This is the Test Component</p>
-      <span>I am Hazhar Maroufi</span>
-    </div>
+    <>
+      <div>
+        <p>This is the Test Component</p>
+        <span>I am Hazhar Maroufi</span>
+        <p>{Math.round(Math.random() * 100)}</p>
+      </div>
+      <div>
+        <p>This is the Test Component</p>
+        <span>I am Hazhar Maroufi</span>
+        <p>{Math.round(Math.random() * 100)}</p>
+      </div>
+    </>
   );
 }
 export default Test;
