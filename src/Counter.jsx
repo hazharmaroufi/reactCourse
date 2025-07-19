8d8dda94d07b5a652a6ad34c7dbd31a4166f8ec5
Hazhar Maroufi <hazhmar@gmail.com>
2025-07-19 19:18:54 +0330

    useState basics

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,14 +4,24 @@ const Counter = () => {
   const [counter, setCounter] = useState(0);
 
   function increaseHandler() {
-    setCounter(counter + 1);
+    setCounter((counter) => counter + 1);
+  }
+  function increaseHandler3() {
+    // setCounter(counter + 3)
+    setCounter((counter) => counter + 1);
+    setCounter((counter) => counter + 1);
+    setCounter((counter) => counter + 1);
+  }
+  function stopHandler() {
+    setCounter(0);
   }
   return (
     <div>
       <h1>Counter</h1>
       <p>{counter}</p>
-      <button onClick={increaseHandler}>Increase</button>
-      {/* <button onClick={() => console.log("increase")}>Increase</button> */}
+      <button onClick={increaseHandler}>Increase 1</button>
+      <button onClick={increaseHandler3}>Increase 3</button>
+      <button onClick={stopHandler}>Stop</button>
     </div>
   );
 };
