bab6fad9dd2cef0f5f12a5d3c4966d36d3263c84
Hazhar Maroufi <hazhmar@gmail.com>
2025-07-19 19:37:22 +0330

    end foundamental for useState hook

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
+  const [isShow, setIsShow] = useState(true);
 
   function increaseHandler() {
     setCounter((counter) => counter + 1);
@@ -15,14 +16,27 @@ const Counter = () => {
   function stopHandler() {
     setCounter(0);
   }
+  function showHandler() {
+    setIsShow(true);
+  }
+  const hideHandler = () => {
+    setIsShow(false);
+  };
+
   return (
-    <div>
-      <h1>Counter</h1>
-      <p>{counter}</p>
-      <button onClick={increaseHandler}>Increase 1</button>
-      <button onClick={increaseHandler3}>Increase 3</button>
-      <button onClick={stopHandler}>Stop</button>
-    </div>
+    <>
+      <button onClick={showHandler}>Show</button>
+      <button onClick={hideHandler}>Hide</button>
+      {isShow && (
+        <div>
+          <h1>Counter</h1>
+          <p>{counter}</p>
+          <button onClick={increaseHandler}>Increase 1</button>
+          <button onClick={increaseHandler3}>Increase 3</button>
+          <button onClick={stopHandler}>Stop</button>
+        </div>
+      )}
+    </>
   );
 };
 export default Counter;
