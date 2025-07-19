f570a80688e91915774aca4867b5fdde8e061e73
Hazhar Maroufi <hazhmar@gmail.com>
2025-07-19 18:54:43 +0330

    practice useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const Counter = () => {
-  function increaseHandler() {}
+  const [counter, setCounter] = useState(0);
+
+  function increaseHandler() {
+    setCounter(counter + 1);
+  }
   return (
     <div>
       <h1>Counter</h1>
+      <p>{counter}</p>
       <button onClick={increaseHandler}>Increase</button>
+      {/* <button onClick={() => console.log("increase")}>Increase</button> */}
     </div>
   );
 };
